36e5aa9685645dda4448fd40d864119d03bcf165
Alberto Alvarez <albertoealvarezm1992@gmail.com>
2017-01-23 01:00:22 -0500

    Server npm packages and PORT. Created data base

---
+++ data-base/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burger (
+	id int AUTO_INCREMENT,
+	name varchar(30) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+
+
+
---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+
+var app = express(); 
+
+var PORT = process.env.PORT || 8080 ;
+
+ 
+
