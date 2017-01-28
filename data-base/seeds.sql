b465cca070d34e9ca6ec82dcedc37656978ec5c8
Alberto Alvarez <albertoealvarezm1992@gmail.com>
2017-01-28 00:24:16 -0500

    seeds updated

--- data-base/seeds.sql
+++ data-base/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burder_db.burger (Name, Devour) VALUES ("Cheese Burger",0  );
-INSERT INTO burder_db.burger (Name, Devour) VALUES ("Double bacon cheese Burger",0);
-INSERT INTO burder_db.burger (Name, Devour) VALUES ("Chicken sandwich",0);
-INSERT INTO burder_db.burger (Name, Devour) VALUES ("Double meat bacon cheese burger",0); 
+INSERT INTO burder_db.burger (Name, Devour, Date) VALUES ("Cheese Burger", 0, NOW() );
+INSERT INTO burder_db.burger (Name, Devour, Date) VALUES ("Double bacon cheese Burger", 0, NOW());
+INSERT INTO burder_db.burger (Name, Devour, Date) VALUES ("Chicken sandwich", 0, NOW());
+INSERT INTO burder_db.burger (Name, Devour, Date) VALUES ("Double meat bacon cheese burger", 0, NOW());
